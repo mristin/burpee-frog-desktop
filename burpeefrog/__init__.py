c5c06c248b996f0a120988dc06829dc27065acda
Marko Ristin <marko@ristin.ch>
2023-01-02 10:10:48 +0100

    Release 0.0.2 (#5)
    * Add pulsating heart (#4)
    * Allow for multiple trunks (#3)
    * Allow for double-jumps (#2)

--- burpeefrog/__init__.py
+++ burpeefrog/__init__.py
@@ -1,6 +1,6 @@
 """Jump burpees and lead the frog to its beloved one."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Marko Ristin"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Production/Stable"
